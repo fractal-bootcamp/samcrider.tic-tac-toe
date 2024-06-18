aa405752f9205cee46cc322409da547e1c30e74f
SamuelRCrider <sam.crider@aol.com>
2024-06-18 17:07:34 -0400

    socket is now on the same port as server

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,31 +1,15 @@
 import config from "./utils/config";
 import app from "./app";
 
-import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-// initialize socket server on port 3003
-const server = createServer(app);
+const server = app.listen(config.PORT, () => {
+  console.log("listening on port: ", config.PORT);
+});
+
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3003"],
+    origin: [`${process.env.API_URL}`],
   },
   transports: ["websocket"],
-});
-
-// listen to it
-server.listen(3003, () => {
-  console.log("socket server listening at 3003");
-});
-
-// test connection
-io.on("connection", (socket) => {
-  io.emit("message", "hello frontend");
-  socket.on("disconnect", () => {
-    io.emit("message", "goodbye frontend");
-  });
-});
-
-app.listen(config.PORT, () => {
-  console.log("listening on port: ", config.PORT);
 });
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         changeOrigin: true,
       },
       "/socket.io": {
-        target: "http://localhost:3003",
+        target: "http://localhost:3004",
         ws: true,
       },
     },
