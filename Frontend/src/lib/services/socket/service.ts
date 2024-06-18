a5f834586a75d8276f797863556ef12abf65e693
SamuelRCrider <sam.crider@aol.com>
2024-06-18 18:36:42 -0400

    hardcode for deployment

--- Frontend/src/lib/services/socket/service.ts
+++ Frontend/src/lib/services/socket/service.ts
@@ -1,6 +1,11 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io(`${process.env.SOCKET_URL}`, {
+const backupUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5173"
+    : "https://backend-tictactoe-rbv1.onrender.com";
+
+const socket: Socket = io(`${process.env.SOCKET_URL || backupUrl}`, {
   transports: ["websocket"],
 });
 
